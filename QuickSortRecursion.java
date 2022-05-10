efb318e5b85e882ba06e4b2583fbb687da8d550a
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2022-05-10 07:44:55 -0700

    Update QuickSortRecursion.java

--- QuickSortRecursion.java
+++ QuickSortRecursion.java
@@ -1,21 +1,3 @@
-/*
-	To apply the method explained in the hint video, we have to add two more parameters in the function call. This can be done by calling a helper function from given function. The helper function can have three parameters: array, start index, end index.  
-	
-	Skeleton code to achieve this:
-	public class Solution {
-		public static void quickSort(int[] input, int startIndex, int endInedx) {
-			// your code goes here
-		}
-	
-		public static void quickSort(int[] input) {
-			quickSort(input, 0, input.length - 1);
-		}
-	
-	}
-	
-*/
-
-
 public class Solution {
     
     public static void quick(int arr[], int low , int high){
@@ -60,12 +42,7 @@ public class Solution {
     }
 	
 	public static void quickSort(int[] arr) {
-		/* Your class should be named Solution
-		 * Don't write main().
-		 * Don't read input, it is passed as function argument.
-		 * No need to print or return the output.
-		 * Taking input and printing output is handled automatically.
-		 */
+	
         
         quick(arr, 0, arr.length-1);
 		
