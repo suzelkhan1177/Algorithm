980af1c5b0bdc748a0832962e32190c645bfcf5b
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2022-05-11 04:29:47 -0700

    Update StringRecursion.java

--- StringRecursion.java
+++ StringRecursion.java
@@ -43,5 +43,26 @@ public class Solution {
         }
 
 	}
+	
+public static boolean checkAB(String input) {
+	
+// Sample Input 1 :
+// abb
+// Sample Output 1 :
+// true
+        
+        if(input.length() == 0){
+            return true;
+        }
+        
+      if(input.charAt(0) == 'a'){
+          if(input.substring(1).length() > 1 && input.substring(1,3).equals("bb")){
+              return checkAB(input.substring(3));
+          }else{
+               return checkAB(input.substring(1));
+          }
+      }
+        return false;
+	}
 
 }
